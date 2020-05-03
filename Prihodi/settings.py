e72cef39b2f0758a2139478c6605d138185662e3
Nurzhan097 <nnnurjan097@gmail.com>
2020-05-03 14:51:31 +0600

    urls

--- Prihodi/settings.py
+++ Prihodi/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = ['127.0.0.1', ]
 # Application definition
 
 INSTALLED_APPS = [
+    'account',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'party'
 ]
 
 MIDDLEWARE = [
